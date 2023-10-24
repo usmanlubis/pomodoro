96009ed62142599e3aced8562a02f62275fe90f8
usmanlubis <usmanlubis11@gmail.com>
2023-10-24 15:08:48 +0700

    move modal to root element of page

--- src/app/components/pomodoro/Pomodoro.tsx
+++ src/app/components/pomodoro/Pomodoro.tsx
@@ -1,13 +1,9 @@
 'use client';
-
 import { useContext } from 'react';
 
 import Timer from './Timer';
 import InputForm from './InputForm';
 import Buttons from './Buttons';
-import SessionEndModal from '../modal/SessionEndModal';
-import RestEndModal from '../modal/RestEndModal';
-import PomodoroEndModal from '../modal/PomodoroEndModal';
 import PomodoroContext from '@/app/context/PomodoroContext';
 
 import styles from './Pomodoro.module.css';
@@ -18,9 +14,6 @@ export default function Pomodoro() {
 	return (
 		<section className={styles.pomodoro}>
 			<p>{ctx.message}</p>
-			{ctx.openSessionEndModal && <SessionEndModal />}
-			{ctx.openRestEndModal && <RestEndModal />}
-			{ctx.openPomodoroEndModal && <PomodoroEndModal />}
 			<Timer />
 			{ctx.isInitialized ? <Buttons /> : <InputForm />}
 			<audio ref={ctx.audioRef} src="/audio/bell.mp3" />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,28 @@
+'use client';
+import { useContext } from 'react';
+
 import Header from './components/header/Header';
 import Pomodoro from './components/pomodoro/Pomodoro';
 import Footer from './components/footer/Footer';
 import { PomodoroContextProvider } from './context/PomodoroContext';
+import SessionEndModal from './components/modal/SessionEndModal'
+import RestEndModal from './components/modal/RestEndModal';
+import PomodoroEndModal from './components/modal/PomodoroEndModal';
+import PomodoroContext from '@/app/context/PomodoroContext';
 
 export default function Home() {
+  const ctx = useContext(PomodoroContext);
+
   return (
-    <>
+    <PomodoroContextProvider>
+      {ctx.openSessionEndModal && <SessionEndModal />}
+			{ctx.openRestEndModal && <RestEndModal />}
+			{ctx.openPomodoroEndModal && <PomodoroEndModal />}
       <Header />
       <main>
-        <PomodoroContextProvider>
-          <Pomodoro />
-        </PomodoroContextProvider>
+        <Pomodoro />
       </main>
       <Footer />
-    </>
+    </PomodoroContextProvider>
   )
 }
