1d9cb3c6fa08a9376432488e1d737cabbfa02d48
usmanlubis <usmanlubis11@gmail.com>
2023-10-26 09:57:44 +0700

    add link to code

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,13 +1,25 @@
 import Link from 'next/link';
 
+import { HiOutlineCode } from 'react-icons/hi'
 import styles from './Header.module.css';
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1>
-        <Link href="/" className={styles['header-brand']}>Pomodoro</Link>
-      </h1>
+      <section>
+        <h1>
+          <Link href="/" className={styles['header-brand']}>Pomodoro</Link>
+        </h1>
+      </section>
+      <nav>
+        <ul className={styles['nav-links']}>
+          <li>
+            <a href="https://github.com/usmanlubis/pomodoro" target="_blank" rel="follow" className={styles['nav-link']} title="View code">
+              <HiOutlineCode />
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
