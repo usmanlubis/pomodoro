0c25dbf5fbd7fda382380d135ff74e89c339ae70
usmanlubis <usmanlubis11@gmail.com>
2023-10-26 10:28:15 +0700

    clean up code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,10 @@ import Header from './components/header/Header';
 import Pomodoro from './components/pomodoro/Pomodoro';
 import Footer from './components/footer/Footer';
 import { PomodoroContextProvider } from './context/PomodoroContext';
-// import SessionEndModal from './components/modal/SessionEndModal';
-// import RestEndModal from './components/modal/RestEndModal';
-// import PomodoroEndModal from './components/modal/PomodoroEndModal';
-import PomodoroContext from '@/app/context/PomodoroContext';
 
 export default function Home() {
-  const ctx = useContext(PomodoroContext);
-
   return (
     <>
-      {/* {ctx.openSessionEndModal && <SessionEndModal />}
-      {ctx.openRestEndModal && <RestEndModal />}
-      {ctx.openPomodoroEndModal && <PomodoroEndModal />} */}
       <Header />
       <main>
         <PomodoroContextProvider>
